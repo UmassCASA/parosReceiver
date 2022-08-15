68c8d8bc8cdd99263f9cfe8d2b6562eb22b1c84e
hakasapl <hakansaplakog@gmail.com>
2022-08-14 20:42:26 -0600

    improved setup.sh

--- receiver.py
+++ receiver.py
@@ -1,5 +1,37 @@
 import pika
+import json
+import time
 
 def main():
-    conn = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = conn.channel()
+    output_file = "recorded_data/" + time.strftime("%Y%m%d-%H%M") + ".csv"
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='parosLogger')
+
+    def callback(ch, method, properties, body):
+        cur_line = body.decode("utf-8")
+        # remove unicode characters
+        cur_line = cur_line.encode("ascii", "ignore")
+        cur_line = cur_line.decode()
+
+        # write to output
+        with open(output_file, 'a') as f:
+            f.write(cur_line)
+            f.write('\n')
+
+    channel.basic_consume(queue='parosLogger', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- setup.sh
+++ setup.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
-source config.sh
+
+# colored output helper functions
+NC='\e[0m'
+echoGreen() {
+    echo -n -e "\e[0;32m$1${NC}"
+}
+echoYellow() {
+    echo -n -e "\e[0;33m$1${NC}"
+}
+echoRed() {
+    echo -n -e "\e[0;31m$1${NC}"
+}
+
+getRmqPass() {
+    echoYellow "Create password for rabbitmq: "
+    read -s rmq_pass
+    echo ""
+
+    echoYellow "Confirm password for rabbitmq: "
+    read -s rmq_pass_confirm
+    echo ""
+
+    if [ "$rmq_pass" != "$rmq_pass_confirm" ]; then
+        echoRed "Passwords do not match, try again\n"
+        getRmqPass
+    fi
+}
 
 # install rabbitmq server
-apt-get update && apt-get upgrade
-apt-get install erlang rabbitmq-server
+echoGreen "Installing APT Prerequisites...\n"
+apt-get update && apt-get -y upgrade
+apt-get -y install erlang rabbitmq-server python3-pip
 
 # enable and start rabbitmq server
+echoGreen "Starting rabbitmq server...\n"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
 # enable rabbitmq management plugin
 #rabbitmq-plugins enable rabbitmq_management
 
-rabbitmqctl add_user $rabbitmq_user $rabbitmq_pass
-rabbitmqctl set_user_tags $rabbitmq_user administrator
-rabbitmqctl set_permissions -p / $rabbitmq_user "." "." "."
+echoYellow "Pick a username for rabbitmq [paros]: "
+read rmq_user
+rmq_user=${rmq_user:-paros}
+
+getRmqPass
+
+rabbitmqctl add_user $rmq_user $rmq_pass
+rabbitmqctl set_user_tags $rmq_user administrator
+rabbitmqctl set_permissions -p / $rmq_user "." "." "."
